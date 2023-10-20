2359f97daeb02b9dd7f01e28d14f4fec5857ebdc
Abraham Velazquez <abraham.velazquez1001@gmail.com>
2023-10-19 21:53:21 -0700

    added some style.css functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const tabs = document.querySelectorAll('nav a');
     const sections = document.querySelectorAll('section');
+    const scrollToTopButton = document.getElementById("scrollToTopButton");
 
     tabs.forEach(tab => {
         tab.addEventListener('click', function(event) {
@@ -13,6 +14,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
             const targetSection = document.getElementById(targetId);
             targetSection.style.display = 'block';
+
+            // Scroll back to the top when a tab is clicked
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
         });
+    });
+
+    // Show the button when the user scrolls down 300px from the top
+    window.addEventListener("scroll", () => {
+        if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+            scrollToTopButton.style.display = "block";
+        } else {
+            scrollToTopButton.style.display = "none";
+        }
+    });
+
+    // Scroll to the top when the button is clicked
+    scrollToTopButton.addEventListener("click", () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
     });
 });
